0a80a69ab37ab5ac596ed151ce813e272b97e1f8
brainysoon <brainysoon@gmail.com>
2018-10-11 23:55:52 +0800

    [responsive] - make the laning page down to 3 seconds.

--- src/landing/reducers/landingPageReducer.js
+++ src/landing/reducers/landingPageReducer.js
@@ -6,14 +6,14 @@ export default (state = {}, action) => {
   switch (action.type) {
     case LANDING_FETCH_LANDING_PAGE: {
       return {
-        counter: 6,
+        counter: 3,
         brand:
           'https://up.enterdesk.com/edpic/76/54/b2/7654b28b1df8bed811dba6e29f3b2840.jpg'
       };
     }
     default: {
       return {
-        counter: 6,
+        counter: 3,
         brand:
           'https://up.enterdesk.com/edpic/76/54/b2/7654b28b1df8bed811dba6e29f3b2840.jpg'
       };
