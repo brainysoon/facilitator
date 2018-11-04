a6c21cea533d07a1a938d13f695a6c621ac02fd9
Shelton Suen <sheltonsuen@gmail.com>
2018-11-04 21:59:22 +0800

    [Shelton] - Rename app to appRoute to make sense.

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 
-import app from './app';
+import appRoute from './appRoute';
 import store from './store';
 import { history } from '../shared/objects/browserObjects';
 
 export default (
   <Provider store={store}>
-    <ConnectedRouter history={history}>{app}</ConnectedRouter>
+    <ConnectedRouter history={history}>{appRoute}</ConnectedRouter>
   </Provider>
 );
